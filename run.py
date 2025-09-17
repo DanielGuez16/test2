fd3fe72a7e4ec4e4a13f05fb089af66a2c9bdf9a
DanielGuez16 <danielguez161101@gmail.com>
2025-09-17 16:27:35 +0200

    ok

--- run.py
+++ run.py
@@ -1613,7 +1613,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=8000,
+        port=8001,
         reload=False,
         log_level="info",
         timeout_keep_alive=600,
